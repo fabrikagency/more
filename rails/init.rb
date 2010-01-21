95d679b9f75556b0466b9de35caf490580206e1b
Caleb Wright <caleb@fabrikagency.com>
2010-01-21 10:45:36 -0500

    Quick fix for option to clean and parse after initialize

--- rails/init.rb
+++ rails/init.rb
@@ -1,7 +1,7 @@
 require File.join(File.dirname(__FILE__), '..', 'lib', 'more')
 
 config.after_initialize {
-  if Less::More.clean_and_parse_after_initialize
+  if Less::More::DEFAULTS[RAILS_ENV][:clean_and_parse_after_initialize]
     Less::More.clean
     Less::More.parse if Less::More.page_cache?
   end
