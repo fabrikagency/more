75527a779b5540d1659c27044f0fcbe9800d5e56
Caleb Wright <caleb@fabrikagency.com>
2010-01-21 11:01:56 -0500

    just disable initialization

--- rails/init.rb
+++ rails/init.rb
@@ -1,8 +1,6 @@
 require File.join(File.dirname(__FILE__), '..', 'lib', 'more')
 
 config.after_initialize {
-  if Less::More.clean_and_parse_after_initialize?
-    Less::More.clean
-    Less::More.parse if Less::More.page_cache?
-  end
+  #Less::More.clean
+  #Less::More.parse if Less::More.page_cache?
 }
